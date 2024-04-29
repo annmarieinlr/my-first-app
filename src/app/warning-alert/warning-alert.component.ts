d5f5c65fc21040a26c28e46b9aed5eb1c85b32e7
annmarieinlr <wor20005@byui.edu>
2024-04-28 22:35:51 -0500

    warning-success

--- src/app/warning-alert/warning-alert.component.ts
+++ src/app/warning-alert/warning-alert.component.ts
@@ -2,7 +2,8 @@ import { Component } from "@angular/core";
 
 @Component({
     selector:'app-warning-alert',
-    templateUrl: './warning-alert.component.html'
+    templateUrl: './warning-alert.component.html',
+    styleUrl: './warning-alert.component.css'
 })
 
 export class WarningAlertComponent {
